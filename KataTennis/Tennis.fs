f742b1f2b7c27d397aa8a9d5dd638bd8c9a1a8ec
Mark Seemann <mark@ploeh.dk>
2015-06-14 09:07:07 +0200

    Returned ended game on ended game.

--- KataTennis/Tennis.fs
+++ KataTennis/Tennis.fs
@@ -1,2 +1,18 @@
 ﻿module Ploeh.Katas.Tennis
 
+type Player = PlayerOne | PlayerTwo
+type Point = Love | Fifteen | Thirty
+
+type PointsData = { PlayerOnePoint : Point; PlayerTwoPoint : Point }
+type FortyData = { Player : Player; OtherPlayerPoint : Point }
+
+type Score =
+| Points of PointsData
+| Forty of FortyData
+| Deuce
+| Advantage of Player
+| Game of Player
+
+// Transitions
+
+let scoreWhenGame winner = Game winner
--- KataTennis/TennisProperties.fs
+++ KataTennis/TennisProperties.fs
@@ -5,3 +5,9 @@ open FsCheck.Xunit
 open Swensen.Unquote
 open Tennis
 
+[<Property>]
+let ``Given game is over then it stays over`` (winner : Player) =
+    let actual : Score = scoreWhenGame winner
+
+    let expected = Game winner
+    expected =! actual
