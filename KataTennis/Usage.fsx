4b87eb59d5e2b19e071bf044cd80e822724278d3
Mark Seemann <mark@ploeh.dk>
2015-11-18 08:29:35 +0100

    Added script file with usage examples.

---
+++ KataTennis/Usage.fsx
@@ -0,0 +1,13 @@
+﻿#load "Tennis.fs"
+
+open Ploeh.Katas.Tennis
+
+let firstBall = score newGame PlayerTwo
+let secondBall = score firstBall PlayerOne
+
+let simpleGame =
+    scoreSeq [
+        PlayerTwo; PlayerTwo; PlayerOne; PlayerTwo; PlayerOne; PlayerOne;
+        PlayerTwo; PlayerTwo]
+
+let displayGame = simpleGame |> scoreToString "Björn Borg" "John McEnroe"
